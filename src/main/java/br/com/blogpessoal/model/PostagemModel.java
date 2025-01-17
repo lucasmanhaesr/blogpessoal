033262b735ba6c32e78a83b828c7f5ed3edaae05
lucasmanhaesr <ramosmanhaes@outlook.com>
2025-01-17 17:50:09 -0300

    Update PostagemModel

--- src/main/java/br/com/blogpessoal/model/PostagemModel.java
+++ src/main/java/br/com/blogpessoal/model/PostagemModel.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import java.time.LocalDateTime;
 
 @Entity(name = "tb_postagem")
